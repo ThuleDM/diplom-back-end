192c09c6f5cdabd3b08de9e7e8377cde470ec3b6
Vladyslav Nikolaiev <vladyslav.nikolaiev@nordstrom.com>
2022-05-28 02:09:17 +0300

    Add image uploading functionality

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+    console.log("HELLLLLOOOOO")
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems, {
         fullWidth: true,
@@ -6,6 +7,8 @@ document.addEventListener('DOMContentLoaded', function() {
         duration: 500
     });
   });
+
+
 
 const toCurrency = price => {
     return new Intl.NumberFormat('de-DE', {
@@ -77,8 +80,9 @@ for (let i = 0; i < dropdowns.length; i++){
 }
 
 //Sort by asc/desc
-document.querySelector('#sort-asc').onclick = mySort;
-document.querySelector('#sort-desc').onclick = mySortDesc;
+//todo uncomment
+// document.querySelector('#sort-asc').onclick = mySort;
+// document.querySelector('#sort-desc').onclick = mySortDesc;
 
 function mySort(){
     let cards = document.querySelector('.main-card');
@@ -112,29 +116,29 @@ function insertAfter(elem, refElem){
     return refElem.parentNode.insertBefore(elem, refElem.nextSibling);
 }
 
-function dragNdrop(event){
+// function dragNdrop(event){
 
-    console.log(event.target.files[0]);
-	var filename = URL.createObjectURL(event.target.files[0]);
-    console.log(filename);
-	var preview = document.getElementById('preview'); 
-	var previewImg = document.createElement("img"); 
-	previewImg.setAttribute("src", filename);
+//     console.log(event.target.files[0]);
+// 	var filename = URL.createObjectURL(event.target.files[0]);
+//     console.log(filename);
+// 	var preview = document.getElementById('preview'); 
+// 	var previewImg = document.createElement("img"); 
+// 	previewImg.setAttribute("src", filename);
 
-	// preview.innerHTML = "";
-	preview.appendChild(previewImg);
+// 	// preview.innerHTML = "";
+// 	preview.appendChild(previewImg);
     
-    for(let i = 0; i < preview.children.length; i++){
-        console.log('HTML : ' + preview.children[i].getAttribute('src'));
-    }
+//     for(let i = 0; i < preview.children.length; i++){
+//         console.log('HTML : ' + preview.children[i].getAttribute('src'));
+//     }
 
-}
+// }
 
-function drag(){
-	document.getElementById('uploadFile').parentNode.className = "draging dragBox";
-}
+// function drag(){
+// 	document.getElementById('uploadFile').parentNode.className = "draging dragBox";
+// }
 
-function drop(){
-	document.getElementById('uploadFile').parentNode.className = 'dragBox';
-}
+// function drop(){
+// 	document.getElementById('uploadFile').parentNode.className = 'dragBox';
+// }
 
--- routes/addProduct.js
+++ routes/addProduct.js
@@ -3,6 +3,18 @@ const Product = require('../models/product');
 const auth = require('../middleware/auth');
 const router = Router();
 
+const path = require("path");
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + path.extname(file.originalname)) //Appending extension
+    }
+  });
+const upload = multer({ storage: storage });
+
 router.get('/', auth, (req, res) => {
     res.render('add', {
         title : 'Add product',
@@ -10,11 +22,15 @@ router.get('/', auth, (req, res) => {
     });
 })
 
-router.post('/', auth, async (req, res) => {
+//creates new product
+router.post('/', auth, upload.array("files"), async (req, res) => {
+    console.log(req.body);
+    console.log(req.files);
+    
     const product = new Product({
         title: req.body.title,
         price: req.body.price,
-        img: req.body.img,
+        img: req.files,
         userId: req.user._id
     })
 
